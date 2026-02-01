295d13d26426bb4662dc1a259e9a2aea29054d4f
DERICK798 <neshderrick798@gmail.com>
2026-02-01 19:29:47 +0300

    Update clientlogin.js

--- js/clientlogin.js
+++ js/clientlogin.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
+
     // 1️⃣ Get form values
     const emailOrPhone = document.getElementById('emailOrPhone').value.trim();
     const password = document.getElementById('password').value;
